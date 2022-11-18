1ad6064bbacbc67b27e160aca0b902ead3e0e521
Lucy Zhou <yixuan25@illinois.edu>
2022-11-18 01:01:23 -0600

    modified:   final_project/src/Graph.h 	renamed:    final_project/lib/nim_graph/edge.h -> final_project/src/edge.h

--- final_project/src/Graph.h
+++ final_project/src/Graph.h
@@ -16,16 +16,15 @@ class Graph{
     class GraphNode{
         public:
             int getNodeId() {return node_id;}
-            std::vector<std::pair<int, int>> getAdjacentList() {return adjacency_list;}
+            std::unordered_map<GraphNode, Edge> getAdjacentList() {return node_adjacency_list;}
 
         private:
             int node_id;
-            std::vector<std::pair<int, int>> adjacency_list;    // vector of <pair: neighbor node, edge weight>
+            std::unordered_map<GraphNode, Edge> node_adjacency_list;
     };
 
     public:
-        Graph(bool weighted);
-        Graph(bool weighted, bool directed);
+        Graph();
 
         /**
          * Returns one vertex in the graph. This function can be used
@@ -50,8 +49,39 @@ class Graph{
          */
         int getEdgeWeight(GraphNode source, GraphNode destination) const;
 
+        /**
+         * Gets an edge between two vertices.
+        * @param source - one vertex the edge is connected to
+        * @param destination - the other vertex the edge is connected to
+        * @return - if exist, return the corresponding edge
+        *         - if edge doesn't exist, return Edge()
+        */
+        Edge getEdge(GraphNode source, GraphNode destination) const;
+
+        /**
+         * Gets all the edges in the graph.
+         * @return a vector of all the edges in the graph
+         */
+        std::vector<Edge> getEdges() const;
+
+        /**
+         * Inserts a new vertex into the graph and initializes its label as "".
+         * @param v - the name for the vertex
+         */
+        void insertGraphNode(GraphNode v);
+
+        /**
+         * Removes a given vertex from the graph.
+         * @param v - the vertex to remove
+         * @return - if v exists, return v
+         *         - if not, return InvalidVertex;
+         */
+        GraphNode removeGraphNode(GraphNode v);
+
+        void clear();
 
     private:
-
+        std::unordered_map<GraphNode, std::unordered_map<GraphNode, Edge>> graph_adjacency_list;
+        // std::unordered_map<GraphNode, string> vertex_label_map;
 
 };
