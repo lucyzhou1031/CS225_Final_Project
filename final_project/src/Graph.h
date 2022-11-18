9fa1078944c7a31138d36ab9fabbdcb2bf1affc3
Lucy Zhou <yixuan25@illinois.edu>
2022-11-17 21:53:44 -0600

    new file:   final_project/.DS_Store 	modified:   final_project/src/Graph.h

---
+++ final_project/src/Graph.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+#include <string>
+
+class Graph{
+    class GraphNode{
+        public:
+
+        private:
+            int node_id;
+            // adjacency
+            
+    };
+
+    public:
+
+
+    private:
+
+};
