9efc78d5d10123aa57e8703e8522d7a80f93c9bf
Lucy Zhou <yixuan25@illinois.edu>
2022-11-18 00:35:01 -0600

    modified:   final_project/lib/nim_graph/edge.h 	modified:   final_project/src/Graph.h 	deleted:    final_project/web-Google.csv

--- final_project/lib/nim_graph/edge.h
+++ final_project/lib/nim_graph/edge.h
@@ -1,6 +1,7 @@
 /**
  * @file edge.h
  * Definition and (minimal) implementation of an edge class.
+ * Adapted from Lab Machine Learning
  */
 
 #pragma once
@@ -8,9 +9,11 @@
 #include <string>
 #include <limits.h>
 
+#include <Graph.h>
+
 using std::string;
 
-typedef string Vertex;
+typedef string GraphNode;
 
 /**
  * Represents an edge in a graph; used by the Graph class.
@@ -21,38 +24,38 @@ typedef string Vertex;
 class Edge
 {
   public:
-    Vertex source; /**< The source of the edge **/
-    Vertex dest; /**< The destination of the edge **/
+    GraphNode source; /**< The source of the edge **/
+    GraphNode dest; /**< The destination of the edge **/
 
     /**
      * Parameter constructor for unweighted graphs.
-     * @param u - one vertex the edge is connected to
-     * @param v - the other vertex it is connected to
+     * @param u - one GraphNode the edge is connected to
+     * @param v - the other GraphNode it is connected to
      */
-    Edge(Vertex u, Vertex v)
+    Edge(GraphNode u, GraphNode v)
         : source(u), dest(v), label(""), weight(-1)
     { /* nothing */
     }
 
     /**
      * Parameter constructor for unweighted graphs.
-     * @param u - one vertex the edge is connected to
-     * @param v - the other vertex it is connected to
+     * @param u - one GraphNode the edge is connected to
+     * @param v - the other GraphNode it is connected to
      * @param lbl - the edge label
      */
-    Edge(Vertex u, Vertex v, string lbl)
+    Edge(GraphNode u, GraphNode v, string lbl)
         : source(u), dest(v), label(lbl), weight(-1)
     { /* nothing */
     }
 
     /**
      * Parameter constructor for weighted graphs.
-     * @param u - one vertex the edge is connected to
-     * @param v - the other vertex it is connected to
+     * @param u - one GraphNode the edge is connected to
+     * @param v - the other GraphNode it is connected to
      * @param w - the weight of the edge
      * @param lbl - the edge label
      */
-    Edge(Vertex u, Vertex v, int w, string lbl)
+    Edge(GraphNode u, GraphNode v, int w, string lbl)
         : source(u), dest(v), label(lbl), weight(w)
     { /* nothing */
     }
--- final_project/src/Graph.h
+++ final_project/src/Graph.h
@@ -1,20 +1,57 @@
+#pragma once
+
 #include <iostream>
 #include <queue>
 #include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
 
+#include "edge.h"
+
+/**
+ * Represents a graph; used by the GraphTools class.
+ */
 class Graph{
     class GraphNode{
         public:
+            int getNodeId() {return node_id;}
+            std::vector<std::pair<int, int>> getAdjacentList() {return adjacency_list;}
 
         private:
             int node_id;
-            // adjacency
-            
+            std::vector<std::pair<int, int>> adjacency_list;    // vector of <pair: neighbor node, edge weight>
     };
 
     public:
+        Graph(bool weighted);
+        Graph(bool weighted, bool directed);
+
+        /**
+         * Returns one vertex in the graph. This function can be used
+         *  to find a random vertex with which to start a traversal.
+         * @return a vertex from the graph
+         * Adapted from Lab Machine Learning
+         */
+        GraphNode getStartingVertex() const;
+        
+        /**
+         * Gets all vertices in the graph.
+         * @return a vector of all vertices in the graph
+         */
+        std::vector<GraphNode> getVertices() const;
+
+        /**
+        * Gets an edge between two vertices.
+         * @param source - one vertex the edge is connected to
+         * @param destination - the other vertex the edge is connected to
+         * @return - if exist, return the corresponding edge
+         *         - if edge doesn't exist, return Edge()
+         */
+        int getEdgeWeight(GraphNode source, GraphNode destination) const;
 
 
     private:
+
 
 };
