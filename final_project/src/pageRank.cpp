2be24e15106541f36067b9578d2d7719407c0744
Cindy4869 <2948292626@qq.com>
2022-11-28 06:01:31 +0000

    finish two functions of PageRank.cpp

--- final_project/src/pageRank.cpp
+++ final_project/src/pageRank.cpp
@@ -1,4 +1,6 @@
 #include <pageRank.h>
+#include <numeric>
+#include <cmath>
 
 PageRank::PageRank(DataParsing data) {
     A_ = data.getTransitMatrix();
@@ -12,11 +14,27 @@ PageRank::PageRank(DataParsing data) {
 }
 
 std::vector<double> PageRank::multiply(std::vector<std::vector<double>> A, std::vector<double> x) {
-
+    std::vector<double> to_return;
+    for (unsigned i = 0; i < A.size(); i++) {
+        double sum = 0;
+        for (unsigned j = 0; j < A.at(i).size(); j++) {
+            sum += A.at(i).at(j) * x.at(j);
+        }
+        to_return.push_back(sum);
+    }
+    return to_return;
 }
 std::vector<double> PageRank::power_iteration(double alpha, double tol, int max_iter) {
 
 }
-double PageRank::norm(std::vector<std::vector<double>> a, std::vector<std::vector<double>> a0) {
-    
+double PageRank::norm(std::vector<double> a, std::vector<double> a0) {
+    if (a.empty() || a0.empty() || a.size() != a0.size()) {
+        return -1;
+    }
+    std::vector<double> diff;
+    for (unsigned i = 0; i < a.size(); i++) {
+        diff.push_back((a.at(i) - a0.at(i)) * (a.at(i) - a0.at(i)));
+    }
+    double norm = std::accumulate(diff.begin(), diff.end(), 0);
+    return sqrt(norm);
 }
--- final_project/src/pageRank.h
+++ final_project/src/pageRank.h
@@ -3,13 +3,15 @@
 class PageRank {
     public:
         PageRank(DataParsing data);
-        std::vector<double> multiply(std::vector<std::vector<double>> A, std::vector<double> x);
+        
         std::vector<double> power_iteration(double alpha, double tol, int max_iter);
-        double norm(std::vector<std::vector<double>> a, std::vector<std::vector<double>> a0);
+        
     private:
         std::vector<std::vector<double>> A_;
         std::vector<double> b_;
         std::vector<double> x_;
         std::vector<std::vector<double>> ones;
+        std::vector<double> multiply(std::vector<std::vector<double>> A, std::vector<double> x);
+        double norm(std::vector<std::vector<double>> a, std::vector<std::vector<double>> a0);
 
 };
